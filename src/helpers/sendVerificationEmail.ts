dff5f48a39cbab039aa896f1fd6ef3431dd890d3
Roshan Kumar Badola <roshanbadola06@gmail.com>
2024-05-07 18:23:04 +0530

    misc

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,13 +1,9 @@
-import {resend} from "@/lib/resend"
+import { resend } from "@/lib/resend"
 import Email from "../../emails/verificationEmail"
 import { ApiResponse } from "@/types/ApiResponse"
 
-interface mailProps{
-email:string,
-username:string,
-}
 
-export async function sendVerificationMail({}){
+export async function sendVerificationMail(email: string, username: string, verifyCode) {
     return;
-} 
+}
 
